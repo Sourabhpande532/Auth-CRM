de349653f23c1e0f69545783ecabc7e6bd23d320
sourabhpande532 <sourabhpande43@gmail.com>
2026-02-05 21:39:17 +0530

    fix: eslint deployment issues

--- anvaya-client-p/src/pages/Agents.jsx
+++ anvaya-client-p/src/pages/Agents.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useAgent } from "../contexts/AgentsContext";
-import { useFetch } from "../userFetch";
 import { url } from "../api";
 import { useAuth } from "../contexts/AuthContext";
 
