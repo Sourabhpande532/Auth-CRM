548eac42a434565df8e6cf484e9f0d9a7f24947b
sourabhpande532 <sourabhpande43@gmail.com>
2026-02-25 22:08:48 +0530

    Feat: filter logic adding and test

--- anvaya-client-p/src/pages/Leads.jsx
+++ anvaya-client-p/src/pages/Leads.jsx
@@ -1,4 +1,9 @@
+import { useAgent } from "../contexts/AgentsContext";
+
 const Leads = () => {
+  const { agents = [] } = useAgent();
+  console.log(agents);
+
   return (
     <div>
       <h2>Leads Page</h2>
