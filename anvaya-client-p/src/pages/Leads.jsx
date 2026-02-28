cd0dfc14fa3d71dd8b0160e32af3904f5282b671
sourabhpande532 <sourabhpande43@gmail.com>
2026-02-28 22:11:07 +0530

    Feat: Add filter logic

--- anvaya-client-p/src/pages/Leads.jsx
+++ anvaya-client-p/src/pages/Leads.jsx
@@ -4,7 +4,7 @@ import { useAgent } from "../contexts/AgentsContext";
 import { useLocation, useNavigate } from "react-router-dom";
 import useLoading from "../useLoading";
 import { useEffect } from "react";
-import { fetchJSON, url } from "../api";
+import { fetchJSON} from "../api";
 import { useAuth } from "../contexts/AuthContext";
 import LeadCard from "../component/leadcard/LeadCard";
 const Leads = () => {
@@ -12,11 +12,31 @@ const Leads = () => {
   const { token } = useAuth();
   const location = useLocation();
   console.log(location);
-  console.log(location.search);
   const navigate = useNavigate();
   const { loading, startLoading, stopLoading } = useLoading();
   const [leads, setLeads] = useState([]);
-  console.log(leads);
+
+  // So this function converts URL into an object or if nothing return ""
+  const getFiltersFromURL = () => {
+    // const sp = new URLSearchParams("?status=New&salesAgent=123");
+    // sp.get('status') i.e New 
+    const params = new URLSearchParams(location.search);
+    return {
+      salesAgent: params.get("salesAgent") || "",
+    };
+  };
+  const [filter, setFilters] = useState(getFiltersFromURL());
+  console.log("State Filter:",filter);
+
+  // useEffect(() => {
+  //   setFilters(getFiltersFromURL());
+  // }, [location.search]);
+
+  const applyFilters = () => {
+    const params = new URLSearchParams();
+    if (filter.salesAgent) params.set("salesAgent", filter.salesAgent);
+    navigate(`/leads?${params.toString()}`);
+  };
 
   useEffect(() => {
     const fetchLeads = async () => {
@@ -33,6 +53,10 @@ const Leads = () => {
     fetchLeads();
     // When url change fetch leads
   }, [location.search]);
+
+  function updateFilterField(field, value) {
+    setFilters((prev) => ({ ...prev, [field]: value }));
+  }
   return (
     <div>
       <h2 className='fw-bold mb-4 text-center text-md-start'>Leads</h2>
@@ -42,7 +66,10 @@ const Leads = () => {
           {/* Sales agent */}
           <div className='col-md-3'>
             <label className='form-label'>Sales Agent</label>
-            <select className='form-control'>
+            <select
+              className='form-control'
+              value={filter.salesAgent}
+              onChange={(e) => updateFilterField("salesAgent", e.target.value)}>
               <option value=''>All</option>
               {agents.map((a) => (
                 <option key={a._id} value={a._id}>
@@ -51,12 +78,14 @@ const Leads = () => {
               ))}
             </select>
           </div>
-          <div className="col-md-2 small">
-          <label className="form-label ">Status</label>
+          <div className='col-md-2 small'>
+            <label className='form-label '>Status</label>
           </div>
           {/* Apply button */}
-          <div className="col-md-1 text-end">
-          <button className="btn btn-primary">Apply</button>
+          <div className='col-md-1 text-end'>
+            <button className='btn btn-primary' onClick={applyFilters}>
+              Apply
+            </button>
           </div>
         </div>
       </div>
